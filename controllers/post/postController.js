52203f907142bc6dedfa6e8bb815129310467193
SEYDOUD <SEYDOUDANSOGO00@gmail.com>
2024-08-08 06:42:30 +0000

    another commit

--- controllers/post/postController.js
+++ controllers/post/postController.js
@@ -35,79 +35,79 @@ exports.create = async (req, res) => {
 
 exports.getAll = async (req, res) => {
     try {
-        // const posts = await Post.aggregate([
-        //     {
-        //         $lookup: {
-        //             from: 'users',
-        //             localField: 'idUser',
-        //             foreignField: '_id',
-        //             as: 'user'
-        //         }
-        //     },
-        //     {
-        //         $lookup: {
-        //             from: 'comments',
-        //             localField: 'comments',
-        //             foreignField: '_id',
-        //             as: 'comments'
-        //         }
-        //     },
-        //     {
-        //         $lookup: {
-        //             from: 'appreciations',
-        //             localField: 'appreciations',
-        //             foreignField: '_id',
-        //             as: 'appreciations'
-        //         }
-        //     },
-        //     {
-        //         $project: {
-        //             _id: 1,
-        //             message: 1,
-        //             img: 1,
-        //             user: { $arrayElemAt: ['$user', 0] },
-        //             appreciationTrueCount: {
-        //                 $size: {
-        //                     $filter: {
-        //                         input: '$appreciations',
-        //                         cond: { $eq: ['$$this.isAppreciate', true] }
-        //                     }
-        //                 }
-        //             },
-        //             appreciationFalseCount: {
-        //                 $size: {
-        //                     $filter: {
-        //                         input: '$appreciations',
-        //                         cond: { $eq: ['$$this.isAppreciate', false] }
-        //                     }
-        //                 }
-        //             },
-        //             commentCount: { $size: '$comments' }
-        //         }
-        //     },
-        //     {
-        //         $sort: { _id: -1 } // Sort by _id in descending order
-        //     }
-        // ]);
+        const posts = await Post.aggregate([
+            {
+                $lookup: {
+                    from: 'users',
+                    localField: 'idUser',
+                    foreignField: '_id',
+                    as: 'user'
+                }
+            },
+            {
+                $lookup: {
+                    from: 'comments',
+                    localField: 'comments',
+                    foreignField: '_id',
+                    as: 'comments'
+                }
+            },
+            {
+                $lookup: {
+                    from: 'appreciations',
+                    localField: 'appreciations',
+                    foreignField: '_id',
+                    as: 'appreciations'
+                }
+            },
+            {
+                $project: {
+                    _id: 1,
+                    message: 1,
+                    img: 1,
+                    user: { $arrayElemAt: ['$user', 0] },
+                    appreciationTrueCount: {
+                        $size: {
+                            $filter: {
+                                input: '$appreciations',
+                                cond: { $eq: ['$$this.isAppreciate', true] }
+                            }
+                        }
+                    },
+                    appreciationFalseCount: {
+                        $size: {
+                            $filter: {
+                                input: '$appreciations',
+                                cond: { $eq: ['$$this.isAppreciate', false] }
+                            }
+                        }
+                    },
+                    commentCount: { $size: '$comments' }
+                }
+            },
+            {
+                $sort: { _id: -1 } // Sort by _id in descending order
+            }
+        ]);
 
-        const posts = await Post.find().option({ maxTimeMS: 30000 });
+        // const posts = await Post.find()
 
-        // const formattedPosts = posts.map(post => ({
-        //     id: post._id,
-        //     message: post.message,
-        //     img: post.img,
-        //     user: post.user.username,
-        //     appreciationTrueCount: post.appreciationTrueCount,
-        //     appreciationFalseCount: post.appreciationFalseCount,
-        //     commentCount: post.commentCount
-        // }));
+        const formattedPosts = posts.map(post => ({
+            id: post._id,
+            message: post.message,
+            img: post.img,
+            user: post.user.username,
+            appreciationTrueCount: post.appreciationTrueCount,
+            appreciationFalseCount: post.appreciationFalseCount,
+            commentCount: post.commentCount
+        }));
 
-        // console.log("posts:",formattedPosts)
+        console.log("posts:",formattedPosts)
         
         if(posts.length == 0){
             res.status(200).send({ callback: "Aucun post pour le moment" });
         }else{
-            res.status(200).send({ callback: posts });
+            res.status(200).send({ callback: formattedPosts });
         }
 
     } catch (error) {
