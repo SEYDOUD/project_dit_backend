713b812dca92cfdda470230accb9e379da609bc9
SEYDOUD <SEYDOUDANSOGO00@gmail.com>
2024-08-08 05:11:10 +0000

    just testing something

--- database/db.js
+++ database/db.js
@@ -6,7 +6,7 @@ const DB_NAME = process.env.DB_NAME;
 
 module.exports = async function (params) {
     try {
-        await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`);
+        await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,{serverSelectionTimeoutMS: 30000,});
         console.log('Connection has been established successfully.');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
