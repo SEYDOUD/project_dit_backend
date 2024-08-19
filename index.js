a3cae057f159541ee7258d731235dd0ad02517c8
SEYDOUD <SEYDOUDANSOGO00@gmail.com>
2024-08-19 09:17:45 +0000

    on teste un autre deployeur

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const helmet = require("helmet")
 const cookieParser = require("cookie-parser")
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://project-dit-frontend.vercel.app');
+    // res.setHeader('Access-Control-Allow-Origin', 'https://project-dit-frontend.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', 'https://funny-crostata-1cb097.netlify.app');
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, X-Custom-Header, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true'); // Autoriser les cookies
